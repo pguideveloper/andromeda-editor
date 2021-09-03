9293e23454aa2e514b71ff2ec257fecfb28f1611
Pedro Henrique Guimarães <pedroguimaraes.contato@gmail.com>
2021-09-03 18:31:03 -0300

    Show current opened file

--- src/editor-config.js
+++ src/editor-config.js
@@ -13,12 +13,26 @@ editor.setSize("100%", "100vh");
 editor.setCursor({line: 1, ch: 1})
 
 ipcRenderer.on('fileName', function(event, data){
+    document.querySelector('.div-bar').textContent = data;
     const fileContent = fs.readFileSync(data).toString(); 
     editor.setValue(fileContent);
 });
 
-ipcRenderer.on('saveFileAs', function(event, filePath) {
+ipcRenderer.on('saveFile', function(event, filePath) {
     fs.writeFileSync(filePath, editor.getValue(), "utf-8");
 });
 
+ipcRenderer.on('saveFileAs', function(event, filePath) {
+    fs.writeFileSync(filePath, editor.getValue(), "utf-8");
+    event.sender.send('receiveFileName', filePath);
+});
+
+
+function handleFilename(filePath) {
+   
+}
+
+function handleFileExtension() {
+
+}
 
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const { app, BrowserWindow, process, dialog, Menu, ipcMain} = require('electron'
 const path = require('path');
 const fs = require('fs');
 
+let openFilePath = null; 
 
 app.on('ready', function() {
     let window = new BrowserWindow({
@@ -27,29 +28,25 @@ app.on('ready', function() {
                 properties: ['openFile'] 
             });
 
-            console.log(files);
-
             if (!files) return; 
 
             const file = files[0];
-            const fileContent = fs.readFileSync(file).toString(); 
-            
             window.webContents.send('fileName', file);
         },
         saveFileAs: function() {
             dialog.showSaveDialog(window, {title: 'Salvar arquivo'}).then(({ filePath }) => {
+                if (!filePath) return;
                 window.webContents.send('saveFileAs', filePath);
             });
         },
         saveFile: function() {
-           
+           window.webContents.send('saveFile', {});
         }
     };
     
     const custom_menu = require('./menu.js')(actions);
     const menu = Menu.buildFromTemplate(custom_menu);
     Menu.setApplicationMenu(menu);
-
 });
 
 
